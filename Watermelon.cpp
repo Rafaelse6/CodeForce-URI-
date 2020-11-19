30e534de462d714d182a767decfafcdbc1caf23b
Rafael Santos <rafa.05125@gmail.com>
2020-11-19 18:14:28 -0300

    First commit

---
+++ Watermelon.cpp
@@ -0,0 +1,22 @@
+// CodeForce solution =  4.A Watermelon
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+   
+
+        int w;
+        cin >> w;
+        if (w >= 1 && w <= 100 && w != 2 && w % 2 == 0) {
+
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+
+}
