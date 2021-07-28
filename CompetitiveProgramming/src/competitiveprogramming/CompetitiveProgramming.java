810c021a1254e5699dabb7c6320f99134b455771
Rafael Santos <rafa.05125@gmail.com>
2021-07-28 17:14:30 -0300

    Java template for competitive programming - int.

---
+++ CompetitiveProgramming/src/competitiveprogramming/CompetitiveProgramming.java
@@ -0,0 +1,22 @@
+
+//Competitive Programming Template(Java - Int)
+package competitiveprogramming;
+
+import java.util.Scanner;
+
+public class CompetitiveProgramming {
+
+    public static void main(String[] args) {
+       
+        
+        Scanner scanner = new Scanner(System.in);
+       
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+       
+        
+       
+        System.out.println("");
+    }
+    
+}
