67ce9f0be3a56f4e041f0f49f72c98517528e2aa
Rafael Santos <rafa.05125@gmail.com>
2021-08-01 14:41:30 -0300

    URI - Área do Círculo - 1002

---
+++ areacirculo/src/areacirculo/Areacirculo.java
@@ -0,0 +1,19 @@
+package areacirculo;
+import java.util.Scanner;
+
+public class Areacirculo {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        double raio = scanner.nextDouble();
+        double area = 3.14159 * (raio * raio);
+        
+        System.out.println(String.format("A=%.4f", area));
+    }
+    
+}
+
+
