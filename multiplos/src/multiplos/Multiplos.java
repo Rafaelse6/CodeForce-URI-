e9bd92849bd404538771100cb27bf616abea8ec4
Rafael Santos <rafa.05125@gmail.com>
2021-08-10 14:19:41 -0300

    URI - Multiplos - 1044

---
+++ multiplos/src/multiplos/Multiplos.java
@@ -0,0 +1,26 @@
+
+package multiplos;
+import java.util.Scanner;
+
+public class Multiplos {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+         int a = scanner.nextInt();
+         int b = scanner.nextInt();
+         
+         if (a % b == 0 || b % a == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+
+}
+        
+        
+    
+    
+
